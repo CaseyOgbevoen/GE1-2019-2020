2f84347fc7da2193f16910b8240b851c39150efc
Ogbevoen <casey.ogbevoen@sap.com>
2019-11-22 14:05:40 +0000

    casey's changes

---
+++ GE1Examples2019/Assets/AudioAnalyser.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioAnalyser : MonoBehaviour
+{
+    public int framesize = 512;
+    public static float[] samples;
+    public static float[] spectrum;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        samples = new float[framesize];
+        spectrum = new float[framesize];
+
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        audio.GetOutputData(samples, 0);
+        audio.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+    }
+}
---
+++ GE1Examples2019/Assets/AudioVis1.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioVis1 : MonoBehaviour
+{
+    public float radius = 20;
+    public float scale = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float theta = (Mathf.PI * 2.0f) / AudioAnalyser.spectrum.Length;
+
+        for(int i=0; i < AudioAnalyser.spectrum.Length; i++)
+        {
+            GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            float angle = theta * i;
+            Vector3 p = new Vector3(
+                Mathf.Sin(angle) * radius
+                , 0
+                , Mathf.Cos(angle) * radius
+                );
+            cube.transform.position = transform.TransformPoint(p);
+            cube.transform.rotation = Quaternion.AngleAxis(angle * Mathf.Rad2Deg, Vector3.up);
+            cube.GetComponent<Renderer>().material.color =
+                Color.HSVToRGB(i / (float)AudioAnalyser.spectrum.Length, 1, 1);
+            cube.transform.parent = this.transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < AudioAnalyser.spectrum.Length; i++)
+        {
+            transform.GetChild(i).localScale = new Vector3(1, 1 + AudioAnalyser.samples[i] * scale, 1);
+        }
+    }
+}
